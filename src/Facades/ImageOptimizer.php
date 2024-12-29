d5481d5142efc72ee9ee4af6ec469db1386eda07
ricardo <elseoclub@gmail.com>
2024-12-29 08:21:41 -0600

    Refactor: Update getFacadeAccessor method to specify return type.

--- src/Facades/ImageOptimizer.php
+++ src/Facades/ImageOptimizer.php
@@ -4,9 +4,13 @@ namespace Grupobahez\Imageoptimizer\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static optimize(string $originalImagePath, array $array)
+ * @method static optimizeFromUrl(string $originalImageUrl, array $array)
+ */
 class ImageOptimizer extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'imageoptimizer';
     }
